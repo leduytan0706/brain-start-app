90365d0f2380be4c703fe057086ed347a2f38fe1
Nhan Phat <162662809+leduytan0706@users.noreply.github.com>
2025-06-21 16:55:54 +0700

    update live.ts

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -8,6 +8,6 @@ export const { sanityFetch, SanityLive } = defineLive({
   client: client.withConfig({ 
     // Live content is currently only available on the experimental API
     // https://www.sanity.io/docs/api-versioning
-    apiVersion: 'vX' 
+    apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION  
   }) 
 });
